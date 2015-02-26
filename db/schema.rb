1ad770dd1a3d055462738e2f9f3bccfa8897f928
Alec Wilson <alec.f.wilson@gmail.com>
2015-02-26 15:39:35 -0800

    Fixes stupid bug and adds Micropost scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150226231726) do
+ActiveRecord::Schema.define(version: 20150226233553) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
